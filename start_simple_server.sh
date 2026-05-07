1044de9c0daa1ef41202fea420cf569ed3554cec
xulilong <xulilong@minrrayav.com>
2026-05-07 17:20:48 +0800

    add client shell

--- start_simple_server.sh
+++ start_simple_server.sh
@@ -1,4 +1,5 @@
 #python asr_server.py --asr-model-path ./models/Qwen3-ASR-1.7B --port 8000
 #python asr_server.py --asr-model-path ./models/Qwen3-ASR-1.7B --port 8000 --debug
-python asr_server.py --asr-model-path ./models/Qwen3-ASR-0.6B --port 8000 --debug
+#python asr_server.py --asr-model-path ./models/Qwen3-ASR-0.6B --port 8000 --debug
 #python asr_server.py --asr-model-path ./models/Qwen3-ForcedAligner-0.6B --port 8000 --debug
+python asr_server.py --asr-model-path ./models/Qwen3-ASR-0.6B --port 8000 --debug --save-audio-dir ./debug_audio --save-audio-mode connection
